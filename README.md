977d1250cee7ef170e920620691d16e2ad8af73c
ZXGuesser <alistair@zxnet.co.uk>
2020-05-27 21:57:27 +0100

    add pid filtering to ts2pes to remove need to pre-filter recordings

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 ## crude scripts to extract teletext data from recorded dvb broadcasts
 
-Use tsp from [TSDuck](https://tsduck.io/) to select only the teletext stream by its PID
-`tsp -I file input.ts -P filter --pid [pid] > out.ts`
+Extract Packetised Elementary Stream data from transport stream
+`python ts2pes.py -i input.ts -p [pid]`
 
-Then extract Packetised Elementary Stream data
-`python ts2pes.py`
-
-Finally extract teletext lines as t42
+Then extract teletext lines as t42
 `python pes2t42.py`
 
 Unused lines are filled with all zeros
--- ts2pes.py
+++ ts2pes.py
@@ -1,11 +1,14 @@
 from functools import partial
 import itertools as it
 import sys
+import click
 
-input = open("out.ts", 'rb')
 output = open("out.pes", 'wb')
 
-def main():
+@click.command()
+@click.option('-i', '--input', type=click.File('rb'), help='Input file.', required=True)
+@click.option('-p', '--pid', type=str, help='PID of teletext stream in hex.', required=True)
+def main(pid, input):
     for packet in iter(partial(input.read, 0xbc), b''):
         if packet[0] != 0x47: # something has gone horribly wrong
             print("packet without sync byte. Aborting")
@@ -13,14 +16,17 @@ def main():
         #TEI = (packet[1] >> 3) & 1
         #PUSI = (packet[1] >> 2) & 1
         #TransportPriority = (packet[1] >> 1) & 1
+        PID = ((packet[1] & 0x1F) << 8) | packet[2]
         
-        #TSC = (packet[3] >> 6) & 3
-        Adaption = (packet[3] >> 4) & 3
-        
-        if Adaption == 1:
-            output.write(packet[4:0xbc])
-        elif Adaption == 3:
-            output.write(packet[5+packet[4]:0xbc])
+        if int(pid, 16) == PID:
+
+            #TSC = (packet[3] >> 6) & 3
+            Adaption = (packet[3] >> 4) & 3
+            
+            if Adaption == 1:
+                output.write(packet[4:0xbc])
+            elif Adaption == 3:
+                output.write(packet[5+packet[4]:0xbc])
 
 if __name__ == "__main__":
     main()
