4b85e603c1ebe1d4ab584cd749085dbb4a3d87ff
ZXGuesser <alistair@zxnet.co.uk>
2020-05-31 16:43:11 +0100

    abort on missing packet in transport stream until I can handle it gracefully

--- ts2pes.py
+++ ts2pes.py
@@ -9,6 +9,8 @@ output = open("out.pes", 'wb')
 @click.option('-i', '--input', type=click.File('rb'), help='Input file.', required=True)
 @click.option('-p', '--pid', type=str, help='PID of teletext stream in hex.', required=True)
 def main(pid, input):
+    ccount = -1
+    
     for packet in iter(partial(input.read, 0xbc), b''):
         if packet[0] != 0x47: # something has gone horribly wrong
             print("packet without sync byte. Aborting")
@@ -27,6 +29,17 @@ def main(pid, input):
                 output.write(packet[4:0xbc])
             elif Adaption == 3:
                 output.write(packet[5+packet[4]:0xbc])
+            
+            Continuity = packet[3] & 0xF
+            
+            if ccount == -1:
+                ccount = Continuity
+            else:
+                ccount = (ccount + 1) & 0xF
+            
+            if ccount != Continuity:
+                print("packet lost at "+hex(input.tell() - 0xbc))
+                return
 
 if __name__ == "__main__":
     main()
